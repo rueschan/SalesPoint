2a5a731a4a6962b0aac666ec9e5e77c329728fa8
rueschan <rescalante21@hotmail.com>
2018-03-06 16:46:39 -0600

    Cambio de menú

--- src/frontend/MainScreen.java
+++ src/frontend/MainScreen.java
@@ -79,11 +79,9 @@ public class MainScreen extends javax.swing.JFrame {
         waiterChoose = new java.awt.Choice();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
+        menusList = new java.awt.List();
+        jLabel6 = new javax.swing.JLabel();
         extrasList = new java.awt.List();
-        button4 = new java.awt.Button();
-        button5 = new java.awt.Button();
-        button6 = new java.awt.Button();
         inventarioPanel = new javax.swing.JPanel();
         MenuBar = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
@@ -211,31 +209,29 @@ public class MainScreen extends javax.swing.JFrame {
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel5.setText("Mesero");
 
-        jPanel1.setLayout(new javax.swing.BoxLayout(jPanel1, javax.swing.BoxLayout.Y_AXIS));
+        menusList.setBackground(new java.awt.Color(204, 204, 204));
+        menusList.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        menusList.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
+        menusList.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menusListActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel6.setFont(new java.awt.Font("Quarca Norm Light", 1, 36)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("Menús");
 
         extrasList.setBackground(new java.awt.Color(204, 204, 204));
         extrasList.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         extrasList.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
-        extrasList.setForeground(new java.awt.Color(102, 102, 102));
-        jPanel1.add(extrasList);
-
-        button4.setBackground(new java.awt.Color(0, 153, 153));
-        button4.setFont(new java.awt.Font("Quarca Norm Regular", 0, 24)); // NOI18N
-        button4.setForeground(new java.awt.Color(51, 51, 51));
-        button4.setLabel("Menú");
-        jPanel1.add(button4);
-
-        button5.setBackground(new java.awt.Color(0, 153, 153));
-        button5.setFont(new java.awt.Font("Quarca Norm Regular", 0, 24)); // NOI18N
-        button5.setForeground(new java.awt.Color(51, 51, 51));
-        button5.setLabel("Menú");
-        jPanel1.add(button5);
-
-        button6.setBackground(new java.awt.Color(0, 153, 153));
-        button6.setFont(new java.awt.Font("Quarca Norm Regular", 0, 24)); // NOI18N
-        button6.setForeground(new java.awt.Color(51, 51, 51));
-        button6.setLabel("Menú");
-        jPanel1.add(button6);
+        extrasList.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                extrasListActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout ventasPanelLayout = new javax.swing.GroupLayout(ventasPanel);
         ventasPanel.setLayout(ventasPanelLayout);
@@ -257,7 +253,9 @@ public class MainScreen extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(ventasPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(menusList, javax.swing.GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE)
+                    .addComponent(extrasList, javax.swing.GroupLayout.DEFAULT_SIZE, 396, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(ventasPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(btnFinalizar, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -291,7 +289,12 @@ public class MainScreen extends javax.swing.JFrame {
                     .addComponent(bebidasList, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(antojosList, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(cartaList, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(ventasPanelLayout.createSequentialGroup()
+                        .addComponent(extrasList, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(menusList, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
 
@@ -374,6 +377,14 @@ public class MainScreen extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_btnFinalizarActionPerformed
 
+    private void menusListActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menusListActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_menusListActionPerformed
+
+    private void extrasListActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_extrasListActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_extrasListActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -442,9 +453,6 @@ public class MainScreen extends javax.swing.JFrame {
     private java.awt.List bebidasList;
     private java.awt.Button btnCancelar;
     private java.awt.Button btnFinalizar;
-    private java.awt.Button button4;
-    private java.awt.Button button5;
-    private java.awt.Button button6;
     private java.awt.List cartaList;
     private java.awt.List extrasList;
     private javax.swing.JPanel inventarioPanel;
@@ -453,13 +461,14 @@ public class MainScreen extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JLabel logo;
+    private java.awt.List menusList;
     private java.awt.TextArea reciboTextArea;
     private javax.swing.JPanel ventasPanel;
     private java.awt.Choice waiterChoose;
